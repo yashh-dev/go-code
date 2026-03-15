b25a5d56418d10b2c4c6f2c8f97cfbee49e97a09
yashh-dev <yashwanthkrishna5027@gmail.com>
2026-03-15 10:21:44 +0530

    chanels

--- channels/main.go
+++ channels/main.go
@@ -6,6 +6,24 @@ import (
 )
 
 func main() {
+	a := gen(1, 2, 34, 5)
+	squared := sq(a)
+	fmt.Println(<-squared)
+	fmt.Println(<-squared)
+	fmt.Println(<-squared)
+	select {
+	case value, ok := <-a:
+		if ok {
+			fmt.Println(value)
+		} else {
+			fmt.Println("im sorry a")
+		}
+	default:
+		fmt.Println("we are sorry")
+	}
+}
+
+func main1() {
 	a := gen(1, 2, 3)
 	fmt.Println(<-a)
 	fmt.Println(<-a)
@@ -19,7 +37,19 @@ func gen(nums ...int) <-chan int {
 	go func() {
 		for _, num := range nums {
 			out <- num
+			fmt.Println(num, "sent")
 			time.Sleep(5 * time.Second)
+		}
+	}()
+	return out
+}
+
+func sq(nums <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for num := range nums {
+			fmt.Println("sq received ", num)
+			out <- num * num
 		}
 	}()
 	return out
